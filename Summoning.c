9b52a683ab564e28ec0735aa1e3d2ad2c6638d34
Pardal24 <126099112+Pardal24@users.noreply.github.com>
2023-03-05 23:23:56 +0000

    Add files via upload

--- Summoning.c
+++ Summoning.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <errno.h>
+#include <netdb.h>
 
 #define MAX_BUFFER_SIZE 1024
 
@@ -30,16 +31,87 @@ char *My_IP()
     return (inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
 }
 
+char *udp_communication(char *IP_server, char *port, char *msg)
+{
+    struct addrinfo hints, *res;
+    int fd, errcode;
+    ssize_t n;
+    fd = socket(AF_INET, SOCK_DGRAM, 0); // UDP socket
+    if (fd == -1)                        /*error*/
+        exit(1);
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;      // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP socket
+    errcode = getaddrinfo(IP_server, port, &hints, &res);
+    if (errcode != 0) /*error*/
+        exit(1);
+    n = sendto(fd, msg, strlen(msg), 0, res->ai_addr, res->ai_addrlen); // msg = NODES 075
+    if (n == -1)                                                        /*error*/
+        exit(1);
+
+    struct sockaddr addr;
+    socklen_t addrlen;
+    char buffer[128 + 1];
+    char host[NI_MAXHOST], service[NI_MAXSERV]; // consts in <netdb.h>
+    /*...*/                                     // see previous task code
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0, &addr, &addrlen);
+    if (n == -1) /*error*/
+        exit(1);
+    buffer[n] = '\0';
+    printf("echo: %s\n", buffer);
+    close(fd);
+    return buffer;
+}
+
+join(char *net, int id, char *regIP, char *regUDP)
+{
+    char *msg_send, *msg_recv;
+    sprintf(msg_send, "NODES %s", net);
+    msg_recv = udp_communication(*regIP, *regUDP, msg_send);
+    printf("%s\n", msg_recv);
+}
+
+int tcp_listener(int port)
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1)
+    {
+        perror("Error creating socket");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        perror("Error binding socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(fd, SOMAXCONN) == -1)
+    {
+        perror("Error listening on socket");
+        exit(EXIT_FAILURE);
+    }
+
+    return fd;
+}
+
 int main(int argc, char *argv[])
 {
-    char *Ip, *TCP, *regIP, *regUDP, *type;
-    fd_set rfds;
-    int fd_listen, newfd, n;
+    char *Ip, *type, *reg_ip, *action, *net, *bootIP, *name; // Passar mrds para estruturas
+    char buffer[MAX_BUFFER_SIZE];
+    int fd_listen, newfd, n, TCP_port;
+    int client_count = 0, maxfd = 0, counter, i, id, bootid, bootTCP, reg_udp;
     int *client_fds = NULL;
     struct sockaddr_in addr;
+    fd_set rfds;
     socklen_t addrlen;
-    char buffer[MAX_BUFFER_SIZE];
-    int client_count = 0, maxfd = 0, counter, i;
 
     if (argc != 5)
     {
@@ -48,29 +120,32 @@ int main(int argc, char *argv[])
     }
     else
     {
+        Ip = argv[1];
+        TCP_port = atoi(argv[2]);
+        reg_ip = argv[3];
+        reg_udp = atoi(argv[4]);
+
         if (strcmp(argv[1], My_IP()) != 0) // Caso o utilizador não saiba o seu id devolver o id dele
         {
             printf("Your IP is: %s\n", My_IP());
             exit(0);
         }
-        if (strcmp(argv[3], "reg193.136.138.142") != 0)
+        if (strcmp(reg_ip, "193.136.138.142") != 0)
         {
-            printf("Please incert in regIP: reg193.136.138.142\n");
+            printf("Please incert in regIP: 193.136.138.142\n");
             exit(0);
         }
-        if (strcmp(argv[4], "reg59000") != 0)
+        if (strcmp(argv[4], "59000") != 0)
         {
-            printf("Please incert in regUDP: reg59000\n");
+            printf("Please incert in regUDP: 59000\n");
             exit(0);
         }
-        Ip = argv[1];
-        TCP = argv[2];
-        regIP = argv[3];
-        regUDP = argv[4];
 
         printf("Welcome to the server please select your next command from this list:\n -join net id\n -djoin net id bootid bootIP bootTCP\n"
                " -create name\n -delete name\n -get dest name\n -show topology (st)\n -show names (sn)\n -show routing (sr)\n -leave\n -exit\n");
     }
+
+    fd_listen = tcp_listener(TCP_port);
 
     while (1)
     {
@@ -92,11 +167,13 @@ int main(int argc, char *argv[])
 
         if (counter <= 0) /*error*/
             exit(1);
+
         while (counter > 0)
         {
 
             if (FD_ISSET(fd_listen, &rfds))
             {
+                FD_CLR(fd_listen, &rfds);
                 addrlen = sizeof(addr);
                 if ((newfd = accept(fd_listen, (struct sockaddr *)&addr, &addrlen)) == -1) /*error*/
                     exit(EXIT_FAILURE);
@@ -105,6 +182,7 @@ int main(int argc, char *argv[])
 
                 client_fds = (int *)realloc(client_fds, (client_count + 1) * sizeof(int));
                 client_fds[client_count++] = newfd;
+                // falta comparar com o maxfd
                 counter--;
             }
             else if (FD_ISSET(STDIN_FILENO, &rfds))
@@ -117,34 +195,57 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
+                    scanf("%s", action);
+                    // VAI PARA UMA FUNCAO
+                    if (strcmp(action, "join") == 0)
+                    {
+                        scanf("%s %d", net, &id);
+                        join(net, id, 0, 0); // Falta ainda perceber onde raio vai perguntar sobre os nos que estao la
+                    }
+                    else if (strcmp(action, "djoin") == 0)
+                    {
+                        scanf("%s %d %d %s %d", net, &id, &bootid, bootIP, &bootTCP);
+                        djoin(net, id, 0, 0);
+                    }
+                    else if (strcmp(action, "create") == 0)
+                    {
+                        scanf("%s", name);
+                    }
+                    else
+                    {
+                        printf("Invalid input\n");
+                    }
                     close(STDIN_FILENO);
                     counter--;
                 }
             }
             else
             {
-                for (i = 0; i < client_count; i++)
+                for (; counter > 0; counter--)
                 {
-                    if (FD_ISSET(client_fds[i], &rfds))
+                    for (i = 0; i < client_count; i++)
                     {
-                        n = read(client_fds[i], buffer, MAX_BUFFER_SIZE);
-                        if (n == -1)
+                        if (FD_ISSET(client_fds[i], &rfds))
                         {
-                            perror("read");
-                            exit(EXIT_FAILURE);
+                            n = read(client_fds[i], buffer, MAX_BUFFER_SIZE);
+                            if (n == -1)
+                            {
+                                perror("read");
+                                exit(EXIT_FAILURE);
+                            }
+                            if (n == 0)
+                            {
+                                printf("Connection closed by client on socket %d\n", client_fds[i]);
+                                close(client_fds[i]);
+                                client_fds[i] = -1;
+                            }
+                            else
+                            {
+                                buffer[n] = '\0';
+                                printf("Received message from client on socket %d: %s\n", client_fds[i], buffer);
+                            }
+                            counter--;
                         }
-                        if (n == 0)
-                        {
-                            printf("Connection closed by client on socket %d\n", client_fds[i]);
-                            close(client_fds[i]);
-                            client_fds[i] = -1;
-                        }
-                        else
-                        {
-                            buffer[n] = '\0';
-                            printf("Received message from client on socket %d: %s\n", client_fds[i], buffer);
-                        }
-                        counter--;
                     }
                 }
             }
